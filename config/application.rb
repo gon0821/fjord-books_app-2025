8e09df81fcf3a6cfc2c34db0b54869696e798ad4
gon821 <gonty821@gmail.com>
2025-10-29 22:46:33 +0900

    add config setting i18n

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module BooksApp
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    I18n.available_locales = [:en, :ja]
+    config.i18n.default_locale = :ja
   end
 end
