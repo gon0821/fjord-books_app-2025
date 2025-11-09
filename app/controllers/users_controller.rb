da0f12bc03e15003376c61be2008a56bded6880d
gon821 <gonty821@gmail.com>
2025-11-09 16:50:39 +0900

    add user details page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,5 +4,6 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params.expect(:id))
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
   root 'books#index'
   devise_for :users
   resources :books
-  get "users/index"
-  get "users/show", as: :user
+  resources :users, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
