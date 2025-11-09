b7827c596b6098f16e707f994522d57ad7a3c039
gon821 <gonty821@gmail.com>
2025-11-09 15:46:08 +0900

    add a user list page

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   root 'books#index'
   devise_for :users
   resources :books
+  get "users/index"
+  get "users/show", as: :user
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
