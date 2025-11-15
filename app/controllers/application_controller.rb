d0e4dd19aff9c5348e38120118729d8d90b77f08
gon821 <gonty821@gmail.com>
2025-11-15 13:29:49 +0900

    Standardize user authentication verification

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,7 @@ class ApplicationController < ActionController::Base
     I18n.locale = :ja
   end
 
+  before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
