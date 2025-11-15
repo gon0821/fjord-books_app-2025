a9cbe681ddb2fc919adaa579ebf3d35200f2b089
gon821 <gonty821@gmail.com>
2025-11-15 11:30:42 +0900

    画像ファイルのカスタムバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ApplicationRecord
   has_one_attached :icon do |attachable|
     attachable.variant :thumb, resize_to_limit: [120, 120]
   end
+  validate :allowed_image_formats
+
+  private
+  def allowed_image_formats
+    if icon.attached? && !icon.content_type.match?(/image\/(jpeg|png|gif)/)
+      errors.add(:icon, 'が正しい画像形式ではありません')
+    end
+  end
 end
