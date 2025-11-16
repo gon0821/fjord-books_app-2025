0ee2fb517123da8497fd6882555fbc51a7a0ba23
gon821 <gonty821@gmail.com>
2025-11-16 19:13:29 +0900

    ルーティングにreportsのリソースを追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
+  resources :books, :reports
   resources :users, only: %i(index show)
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
