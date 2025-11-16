d0fa49db2b1b35d2eb620ee2272230b4b8c5eaaf
gon821 <gonty821@gmail.com>
2025-11-17 01:33:53 +0900

    reportsコントローラを追加

---
+++ app/controllers/reports_controller.rb
@@ -0,0 +1,47 @@
+class ReportsController < ApplicationController
+  before_action :set_report, only: %i[show edit update destroy]
+
+  def index
+    @reports = Report.all
+  end
+
+  def show; end
+
+  def new
+    @report = Report.new
+  end
+
+  def edit; end
+
+  def create
+    @report = Report.new(report_params)
+    if @report.save
+      redirect_to @report, notice: t('controllers.common.notice_create', name: Report.model_name.human)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @report.update(report_params)
+      redirect_to @report, status: :see_other, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @report.destroy!
+    redirect_to reports_path, status: :see_other, t('controllers.common.notice_destroy', name: Report.model_name.human)
+  end
+
+  private
+
+  def set_report
+    @report = Report.find(params.expect(:id))
+  end
+
+  def report_params
+    params.expect(report: %i[title content target_date])
+  end
+end
