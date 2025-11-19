5902e9a14585614edbcfbff4492be1b240e0d920
gon821 <gonty821@gmail.com>
2025-11-20 05:41:20 +0900

    reportsコントローラとビューの修正: 編集・更新・削除時にユーザー制限を追加し、エラーメッセージを表示するフォームを実装

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -11,7 +11,9 @@ class ReportsController < ApplicationController
     @report = current_user.reports.new
   end
 
-  def edit; end
+  def edit
+    return redirect_to reports_path, alert: '自分以外の日報は編集できません' unless @report.user == current_user
+  end
 
   def create
     @report = current_user.reports.new(report_params)
@@ -23,6 +25,7 @@ class ReportsController < ApplicationController
   end
 
   def update
+    return redirect_to reports_path, alert: '自分以外の日報は編集できません' unless @report.user == current_user
     if @report.update(report_params)
       redirect_to @report, status: :see_other, notice: t('controllers.common.notice_update', name: Report.model_name.human)
     else
@@ -31,6 +34,7 @@ class ReportsController < ApplicationController
   end
 
   def destroy
+    return redirect_to reports_path, alert: '自分以外の日報は削除できません' unless @report.user == current_user
     @report.destroy!
     redirect_to reports_path, status: :see_other, notice: t('controllers.common.notice_destroy', name: Report.model_name.human)
   end
