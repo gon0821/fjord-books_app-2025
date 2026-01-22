4c3cfc5b2d4256d9fc580196e635ea9555ffffdf
gon821 <gonty821@gmail.com>
2026-01-22 23:28:25 +0900

    各子コントローラーの先頭に frozen_string_literal を追加

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class BookCommentsController < CommentsController
   def create
     @comment = Comment.new(comment_params)
--- app/controllers/report_comments_controller.rb
+++ app/controllers/report_comments_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ReportCommentsController < CommentsController
   def create
     @comment = Comment.new(comment_params)
