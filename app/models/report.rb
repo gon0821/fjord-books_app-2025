491023e85e0ae23ac117fba8949f8a253efb1f3d
gon821 <gonty821@gmail.com>
2026-02-15 10:53:23 +0900

    モデルアソシエーションの設定

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,12 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :active_mentions, class_name: 'ReportMention', foreign_key: :mentioning_report_id
+  has_many :mentioning_reports, through: :active_mentions, source: :mentioned_report
+
+  has_many :passive_mentions, class_name: 'ReportMention', foreign_key: :mentioned_report_id
+  has_many :mentioned_reports, through: :passive_mentions, source: :mentioning_report
+
   validates :title, presence: true
   validates :content, presence: true
 
