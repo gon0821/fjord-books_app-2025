038bd8592f72776fd1d044eb00a85102d97436c6
gon821 <gonty821@gmail.com>
2026-02-21 15:05:17 +0900

    report削除時にmentionも削除されるようにする

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,7 +5,7 @@ class Report < ApplicationRecord
   has_many :comments, as: :commentable, dependent: :destroy
 
   has_many :active_mentions, class_name: 'ReportMention', foreign_key: :mentioning_report_id
-  has_many :mentioning_reports, through: :active_mentions, source: :mentioned_report
+  has_many :mentioning_reports, through: :active_mentions, source: :mentioned_report, dependent: :destroy
 
   has_many :passive_mentions, class_name: 'ReportMention', foreign_key: :mentioned_report_id
   has_many :mentioned_reports, through: :passive_mentions, source: :mentioning_report
